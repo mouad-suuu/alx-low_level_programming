c8cc91fd8f9aea7cbb730b5fa223557e119b6016
mouad-suuu <moad.menniui@gmail.com>
2023-09-14 15:26:38 -0700

    task 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,16 +9,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *ptr = h;
 	size_t n = 0;
 
-	while (ptr && ptr->prev)
-		ptr = ptr->prev;
-	while (ptr)
+	while (h)
 	{
-		printf("%d\n", ptr->n);
+		printf("%d\n", h->n);
 		n++;
-		ptr = ptr->next;
+		h = h->next;
 	}
 	return (n);
 }
