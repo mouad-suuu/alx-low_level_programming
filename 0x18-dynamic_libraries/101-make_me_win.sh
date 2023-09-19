3cbcade39c508a8b74e9d94aea92ba25bc23c1b1
mouad-suuu <moad.menniui@gmail.com>
2023-09-18 17:29:07 -0700

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P /tmp 
+wget -P /tmp https://github.com/mouad-suuu/alx-low_level_programming/blob/5870bc66237d2e702f0246361d306a58dc344aab/0x18-dynamic_libraries/libgiga.so 
+export LD_PRELOAD=/tmp/libgiga.so
