8a208e1b7297a1fb48c6501e8add00d37ea21ef2
mouad-suuu <moad.menniui@gmail.com>
2023-09-18 17:51:58 -0700

    task 3 trying an other way

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/mouad-suuu/alx-low_level_programming/blob/5870bc66237d2e702f0246361d306a58dc344aab/0x18-dynamic_libraries/lib101-makemewin.so 
+wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/B3zaleel/alx-low_level_programming/raw/main/0x18-dynamic_libraries/lib101-makemewin.so
 export LD_PRELOAD=$HOME/lib101-makemewin.so
